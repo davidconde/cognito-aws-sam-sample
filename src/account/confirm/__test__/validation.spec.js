6da602bc587202076a06cdeb32f2248a517451fc
David Conde Marin <davidmarin@rentalcars.com>
2020-07-25 09:56:18 +0100

    adding base user account confirmation lambda

---
+++ src/account/confirm/__test__/validation.spec.js
@@ -0,0 +1,37 @@
+const {lambdaHandler} = require("../index");
+
+describe("validation tests for account confirmation", () => {
+
+  const createBody = (body) => {
+    return {
+      body: JSON.stringify(body)
+    };
+  }
+
+  it("validates null body being posted and returns 400",  async () => {
+    const response = await lambdaHandler(null, null);
+
+    expect(typeof response).toBe("object");
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("validates no email being posted and returns 400",  async () => {
+    const response = await lambdaHandler(createBody(), null);
+
+    expect(typeof response).toBe("object");
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("validates full body being posted and returns 200",  async () => {
+    
+    const body = {
+      email: "sample.email@email.com",
+      password: "aPassW0rd!",
+    };
+
+    const response = await lambdaHandler(createBody(body), null);
+
+    expect(typeof response).toBe("object");
+    expect(response.statusCode).toBe(200);
+  });
+})
---
+++ src/account/confirm/index.js
@@ -0,0 +1,32 @@
+const { ResponseUtil, BodyParser } = require('dcm-lambda-utils');
+const {setupLogger}  = require("/opt/nodejs/logger.js");
+
+const isValidRequest = (requestBody) => {
+  // insert fancy validation here
+  return (
+    requestBody.email && requestBody.password
+  )
+}
+
+exports.lambdaHandler = async (event, context) => {
+  const logger = setupLogger(context);
+  
+  if (!event || !event.body)
+    return ResponseUtil.Error(400);
+
+  const body = BodyParser(event);
+  
+  // poor man's validation
+  if (body === null || !isValidRequest(body))
+    return ResponseUtil.Error(400);
+  
+  try {
+    logger.info("confirming user account");
+
+    return ResponseUtil.OK({});
+  } 
+  catch (error) {
+    logger.error(error)
+    return ResponseUtil.Error(500, "There was an error saving the user");
+  }
+}
