6e4e714ef2dbbf5b03e8b5adda1e4cc8797560f0
David Conde Marin <davidmarin@rentalcars.com>
2020-08-10 21:29:27 +0100

    refactoring

--- src/demo-auth/index.js
+++ src/demo-auth/index.js
@@ -1,47 +1,6 @@
-const { ResponseUtil } = require('dcm-lambda-utils');
-const {setupLogger, getLogger}  = require("/opt/nodejs/logger.js");
+const {setupLogger}  = require("/opt/nodejs/logger.js");
 const checkCredentials = require("./cognito-service");
-const getTemplateText = require("./template-loader");
-
-const composeResponse = (template) => {
-  const response = ResponseUtil.OK({});
-  response.headers["Content-Type"] = "text/html";
-  response.body = template;
-
-  return response;
-}
-
-const anonymousResponse = () => {
-  const logger = getLogger();
-  logger.info("No authentication provided, returning anonymous header");
-  
-  try {
-    const template = getTemplateText("./header.html", []);
-    return composeResponse(template);
-  } catch (error) {
-    logger.error(error)
-  }
-}
-
-const authenticatedResponse = (user) => {
-  const logger = getLogger();
-  logger.info("user authenticated, returning logged in header");
-
-  const replacements = [];
-
-  if (user) {
-    replacements.push({key: "user", value: user["custom:FullName"]});
-  }
-
-  try {
-    const template = getTemplateText("./header-auth.html", replacements);
-    return composeResponse(template);
-  } catch (error) {
-    logger.error(error);
-    logger.info("fallback to anonymous template");
-    return anonymousResponse();
-  }  
-}
+const {anonymousResponse, authenticatedResponse} = require("./response-processor");
 
 exports.lambdaHandler = async (event, context) => {
   const logger = setupLogger(context);
